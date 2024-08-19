66628c990c7a5adcddc3e8dc5ecf6c837c3e44df
AdharshPS <adharshps000@gmail.com>
2024-08-19 15:58:50 +0530

    lab

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:hms_web_project/presentation/dashboard_screen/view/drawer/admin/
 import 'package:hms_web_project/presentation/dashboard_screen/view/emr/emr.dart';
 import 'package:hms_web_project/presentation/dashboard_screen/view/lab/radiology/lab_details.dart';
 import 'package:hms_web_project/presentation/dashboard_screen/view/pharmacy/billing_pharmcay.dart';
+import 'package:hms_web_project/presentation/dashboard_screen/view/store/controller/controller.dart';
 import 'package:hms_web_project/presentation/splash_screen/splash_screen.dart';
 import 'package:provider/provider.dart';
 
