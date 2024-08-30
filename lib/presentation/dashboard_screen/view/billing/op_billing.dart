71374245a28a3f5755872fc365e2e6a564df328f
akhi0216 <akhilam21@gmail.com>
2024-08-30 13:34:59 +0530

    opbilling ui changed

--- lib/presentation/dashboard_screen/view/billing/op_billing.dart
+++ lib/presentation/dashboard_screen/view/billing/op_billing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hms_web_project/constants/color_constants.dart';
 import 'package:hms_web_project/constants/texts.dart';
 
 class OpBilling extends StatefulWidget {
@@ -9,194 +10,149 @@ class OpBilling extends StatefulWidget {
 }
 
 class _OpBillingState extends State<OpBilling> {
-  final TextEditingController _doctorNameController = TextEditingController();
-  final TextEditingController _patientNameController = TextEditingController();
-  final TextEditingController _wardRoomController = TextEditingController();
-
-  // Boolean to track visibility of details
-  bool _showDetails = false;
-
-  @override
-  void dispose() {
-    _doctorNameController.dispose();
-    _patientNameController.dispose();
-    _wardRoomController.dispose();
-    super.dispose();
-  }
-
-  void _toggleDetails() {
-    setState(() {
-      _showDetails = !_showDetails;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(8.0),
       child: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
-              child: Column(
-                children: [
-                  Text(
-                    "HIGHLAND HOSPITAL RESEARCH AND DIAGNOSTIC CENTRE",
-                    style: MyTextStyle.highlandheading
-                        .copyWith(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    "Mother Theresa Rd, Highlands, Kankanady,",
-                    style: MyTextStyle.highlandaddress.copyWith(fontSize: 16),
-                  ),
-                  Text(
-                    "Mangaluru, Karnataka 575002",
-                    style: MyTextStyle.highlandaddress.copyWith(fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 40),
-            Center(
               child: Text(
-                "RECEIPT",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22),
+                "HIGHLAND HOSPITAL RESEARCH AND DIAGNOSTIC CENTRE",
+                style: MyTextStyle.highlandheading
+                    .copyWith(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
-            Divider(
-              color: Colors.blueAccent,
-              thickness: 2,
+            SizedBox(height: 8),
+            Text(
+              "Mother Theresa Rd, Highlands, Kankanady,",
+              style: MyTextStyle.highlandaddress.copyWith(fontSize: 16),
             ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Text(
+              "Mangaluru, Karnataka 575002",
+              style: MyTextStyle.highlandaddress.copyWith(fontSize: 16),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "CASH BILL(Original)",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text(
-                  "Receipt No : 5",
-                  style: MyTextStyle.normalText,
+                // row 1
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("     Patient No : "),
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: TextFormField(
+                            decoration:
+                                InputDecoration(border: OutlineInputBorder()),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Department : "),
+                        // Text("Doctor:"),
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: TextFormField(
+                            decoration:
+                                InputDecoration(border: OutlineInputBorder()),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-                Text(
-                  "Receipt Date : 23 Dec 2019",
-                  style: MyTextStyle.normalText,
+                SizedBox(
+                  height: 11,
                 ),
+                // row 2
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("Patient Name : "),
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: TextFormField(
+                            decoration:
+                                InputDecoration(border: OutlineInputBorder()),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Bill No : "),
+                        // Text("Doctor:"),
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: TextFormField(
+                            decoration:
+                                InputDecoration(border: OutlineInputBorder()),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 11,
+                ),
+
+                // row 3
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("            Doctor : "),
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: TextFormField(
+                            decoration:
+                                InputDecoration(border: OutlineInputBorder()),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text("Bill Date : "),
+                        // Text("Doctor:"),
+                        Container(
+                          constraints: BoxConstraints(maxWidth: 300),
+                          child: TextFormField(
+                            decoration:
+                                InputDecoration(border: OutlineInputBorder()),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                // row 4
               ],
             ),
-            Divider(
-              color: Colors.blueAccent,
-              thickness: 2,
-            ),
-            SizedBox(height: 20),
-            _buildTextField("Doctor Name:", _doctorNameController),
-            SizedBox(height: 20),
-            _buildTextField("Patient Name:", _patientNameController),
-            SizedBox(height: 20),
-            _buildTextField("Ward/Room No:", _wardRoomController),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _toggleDetails,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueAccent, // Background color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-              ),
-              child: Text(
-                _showDetails ? "Hide Details" : "Print",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 20),
-            Visibility(
-              visible: _showDetails,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildDetailSection(
-                      "Lab Reports:", ['Test : Blood Test', 'Report: Normal']),
-                  SizedBox(height: 20),
-                  _buildDetailSection("Pharmacy Details:",
-                      ['paracetamol: 50g', 'aspirin: 100mg']),
-                ],
-              ),
-            ),
-            SizedBox(height: 40),
-            _buildAmountSection("Amount Received in Cash", "RS.100.00",
-                "Rupees One Hundred Only"),
-            SizedBox(height: 10),
-            _buildAmountSection(
-                "GST (18%)", "RS.18.00", "Rupees Eighteen Only"),
+            // Divider(
+            //   color: ColorConstants.mainBlack,
+            //   // width: 3,
+            // )
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildTextField(String label, TextEditingController controller) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: MyTextStyle.normalText.copyWith(fontSize: 16),
-        ),
-        SizedBox(
-          width: double.infinity,
-          child: TextFormField(
-            controller: controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              contentPadding:
-                  EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildDetailSection(String title, List<String> details) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: MyTextStyle.highlandheading
-              .copyWith(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 10),
-        ...details.map((detail) => Text(detail, style: MyTextStyle.normalText)),
-        SizedBox(height: 10),
-      ],
-    );
-  }
-
-  Widget _buildAmountSection(
-      String label, String amount, String amountInWords) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: MyTextStyle.normalText
-              .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 5),
-        Text(
-          amount,
-          style: TextStyle(
-              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
-        ),
-        Text(
-          amountInWords,
-          style: MyTextStyle.normalText.copyWith(fontSize: 16),
-        ),
-      ],
     );
   }
 }
